2899ff2135624fb1eefc5091c08dc69c85bae20c
zsimo <mohamedzoraa100@gmail.com>
2023-10-02 07:03:47 +0100

    test

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,7 +7,7 @@ const Services = () => {
             <div className="container mx-auto p-4">
                 <div className="relative hide duration-700 transition-[opacity,transform] ">
                     <h1 className="text-5xl md:text-6xl xl:text-7xl  font-extrabold text-[#40dcdf] text-center">Services</h1>
-                    <h2 className="text-3xl lg:text-4xl xl:text-5xl absolute w-full h-fit translate-y-[55%] bottom-0 left-0 font-extrabold text-[#40dcdf]  text-center opacity-30">What We Are Great At</h2>
+                    <h2 className="text-3xl lg:text-4xl xl:text-5xl absolute w-full h-fit translate-y-[55%] bottom-0 left-0 font-extrabold text-[#40dcdf]  text-center opacity-30">What Im Great At</h2>
                 </div>
                 <div className="flex flex-wrap gap-20 justify-center items-center mt-28 ">
                     {services.map((service, id) => <Service key={id} id={id} {...service} />)}
