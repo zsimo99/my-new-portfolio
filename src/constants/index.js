d7aebf4dea558c017656d4e565d04be4941005b9
zsimo <mohamedzoraa100@gmail.com>
2023-10-02 07:30:31 +0100

    addBlogZs

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,19 +8,20 @@ import Promptobia from "../assets/projects/Promptobia.png"
 import dalle from "../assets/projects/dalle.png"
 import todozs from "../assets/projects/todozs.png"
 import rose from "../assets/projects/rose.png"
+import blogzs from "../assets/projects/blogzs.png"
 
 export const projects = [
 
 
-    // {
-    //     title: "BlogZs",
-    //     link: "https://github.com/zsimo99/blog-zs-v2",
-    //     desc: "This project is a basic website template created with HTML and CSS.",
-    //     live: "https://blog-zs-v2.vercel.app/",
-    //     done: true,
-    //     category: "wa",
-    //     image: leon
-    // },
+    {
+        title: "BlogZs",
+        link: "https://github.com/zsimo99/blog-zs-v2",
+        desc: "I'm thrilled to introduce DEVBlog - Empowering Developers to Connect and Collaborate! This project has been a labor of love, designed to create a vibrant hub where coding questions find quick answers, and developers discover a thriving community. With robust authentication, a user-friendly interface adaptable to your preferences, and upcoming chat functionality, DEVBlog promises to be the ultimate platform for all your coding needs. Join us and experience the passion and dedication that went into crafting this developer's haven!",
+        live: "https://blog-zs-v2.vercel.app/",
+        done: true,
+        category: "wa",
+        image: blogzs
+    },
     {
         title: "Rose Template",
         link: "https://github.com/zsimo99/rose",
